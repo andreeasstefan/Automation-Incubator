266b3393d91d8b93d9da0a9a60757db985095ec9
Andreea Mihaela Stefan <Andreea-Mihaela.Stefan@endava.com>
2024-11-04 20:41:16 +0200

    Homework day 17-update

--- src/main/java/com/endava/petclinic/services/DBService.java
+++ src/main/java/com/endava/petclinic/services/DBService.java
@@ -13,30 +13,36 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
 
-public class DBService {
-    private static final Logger LOGGER = LogManager.getLogger(DBService.class);
+public class DBServiceOwner {
+    private static final Logger LOGGER = LogManager.getLogger(DBServiceOwner.class);
     public Owner getOwnerById(Long id) {
         LOGGER.info("Starting SELECT query for owner with ID: {}", id);
-        try(Connection conn = DriverManager.getConnection(EnvReader.getDBUrl(),EnvReader.getDbUsername(),EnvReader.getDbPassword())) {
 
+        String dbUrl = EnvReader.getDBUrl();
+        String dbUsername = EnvReader.getDbUsername();
+        String dbPassword = EnvReader.getDbPassword();
 
+        try (Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
+
+            // Map the database columns to Java properties
             var mapColumnsToProperties = new HashMap<String, String>();
-            //mapping your db to entity here;
-            mapColumnsToProperties.put("first_name","firstName");
-            mapColumnsToProperties.put("last_name","lastName");
+            mapColumnsToProperties.put("first_name", "firstName");
+            mapColumnsToProperties.put("last_name", "lastName");
+
             BeanProcessor beanProcessor = new BeanProcessor(mapColumnsToProperties);
             RowProcessor rowProcessor = new BasicRowProcessor(beanProcessor);
+            ResultSetHandler<Owner> handler = new BeanHandler<>(Owner.class, rowProcessor);
 
-            ResultSetHandler<Owner> h = new BeanHandler<Owner>(Owner.class, rowProcessor);
+            // Execute the query
             QueryRunner runner = new QueryRunner();
-            Owner owner = runner.query(conn, "SELECT * from owners where id = ?", h, id );
+            Owner owner = runner.query(conn, "SELECT * FROM owners WHERE id = ?", handler, id);
+
             LOGGER.info("SELECT query completed for owner with ID: {}. Result: {}", id, owner);
             return owner;
+
         } catch (SQLException e) {
             LOGGER.error("Failed to connect to the database for owner with ID: {}", id, e);
-            throw new RuntimeException("Can t cnnect to DB ",e);
+            throw new RuntimeException("Can't connect to the database", e);
         }
-
     }
-
 }
